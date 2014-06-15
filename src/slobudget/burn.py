53d4660e24099bb1c88973ae42e9a31d7a21c56d
Ryft <10768614+propavingk@users.noreply.github.com>
2014-06-15 09:15:00 -0500

    chore: error-budget

---
+++ src/slobudget/burn.py
@@ -0,0 +1,19 @@
+"""Burn rate over a window and projection to budget exhaustion.
+
+Burn rate is the observed failure ratio in a window divided by the objective's
+allowed failure ratio. A burn rate of 1 spends the budget exactly on schedule
+over the compliance window. A burn rate of 10 spends it ten times as fast.
+
+Projection to exhaustion is deliberately conservative. It assumes the current
+rate continues unchanged, which is a statement about a hypothetical, not a
+prediction. The functions here refuse to project when:
+
+    - the burn rate is zero, because a rate of zero never exhausts anything,
+    - the requested window extends past the recorded data, or the recorded
+      data has a gap inside the window, because then the rate is computed from
+      an incomplete or discontiguous sample.
+
+Callers must treat a projection as conditional on the rate holding.
+"""
+
+from __future__ import annotations
