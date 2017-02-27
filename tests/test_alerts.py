165091c2e0406ffdf037e081e620e0d89518d7cf
Ryft <10768614+propavingk@users.noreply.github.com>
2017-02-27 11:08:00 -0500

    refactor: tidy sli

--- tests/test_alerts.py
+++ tests/test_alerts.py
@@ -19,3 +19,11 @@ class DefaultPolicyTests(unittest.TestCase):
         self.assertEqual(names, ["fast-burn", "slow-burn"])
 
 
+class EvaluateTests(unittest.TestCase):
+    def setUp(self):
+        self.series = load_series("samples/service-a.sli")
+        self.objective = Objective(target=0.999, window_seconds=30 * 86400)
+
+    def test_fast_burn_fires_on_incident(self):
+        evals = evaluate_all(self.series, self.objective)
+        fast = next(e for e in evals if e.policy.name == "fast-burn")
