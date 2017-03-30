123aed61423fb790ad3d01918f5b6e2fe5674825
Ryft <10768614+propavingk@users.noreply.github.com>
2017-03-30 09:03:00 -0500

    chore: bump cli

--- tests/test_objective.py
+++ tests/test_objective.py
@@ -7,3 +7,13 @@ from slobudget.objective import (
     ObjectiveError,
     derive_budget,
     format_duration,
+    parse_objective,
+    parse_window,
+)
+
+
+class ParseObjectiveTests(unittest.TestCase):
+    def test_percent_sign(self):
+        self.assertAlmostEqual(parse_objective("99.9%"), 0.999)
+
+    def test_bare_percentage(self):
